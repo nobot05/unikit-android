6ce86eaa683ae80b2e66afe67ad5918d63458182
mauerbauertraurigkeit <nour20hk@gmail.com>
2021-12-05 12:28:38 +0200

    login

--- app/src/main/java/com/nhk/unikit/MainActivity.java
+++ app/src/main/java/com/nhk/unikit/MainActivity.java
@@ -3,12 +3,17 @@ package com.nhk.unikit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+
     }
 }
