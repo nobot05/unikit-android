1fc0e5aaa024e60fd143c3122963007380bd6b3b
mauerbauertraurigkeit <nour20hk@gmail.com>
2021-12-05 16:12:02 +0200

    login+signup

--- app/src/main/java/com/nhk/unikit/MainActivity.java
+++ app/src/main/java/com/nhk/unikit/MainActivity.java
@@ -2,18 +2,27 @@ package com.nhk.unikit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button loginBtn;
+    Button regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginBtn = (Button) findViewById(R.id.loginBtn);
+        regBtn = (Button) findViewById(R.id.regPage);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivitySignUp.class));
+            }
+        });
 
     }
 }
