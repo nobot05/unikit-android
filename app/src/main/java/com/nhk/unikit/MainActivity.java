322f8b8b0790d28a79cefce1aaa65afa4edf2e9e
mauerbauertraurigkeit <nour20hk@gmail.com>
2021-12-05 23:34:07 +0200

    commit

--- app/src/main/java/com/nhk/unikit/MainActivity.java
+++ app/src/main/java/com/nhk/unikit/MainActivity.java
@@ -10,17 +10,26 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button regBtn;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         regBtn = (Button) findViewById(R.id.regPage);
+        login = (Button) findViewById(R.id.signBtn);
 
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MainActivitySignUp.class));
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ItemsPage.class));
             }
         });
 
