bc58917109ca046b7e9a3da780082f0538519efc
mauerbauertraurigkeit <nour20hk@gmail.com>
2021-12-06 22:30:43 +0200

    commit

---
+++ app/src/main/java/com/nhk/unikit/ItemsPage.java
@@ -0,0 +1,17 @@
+package com.nhk.unikit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+public class ItemsPage extends AppCompatActivity {
+    //ListView
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_page);
+
+    }
+}
