7b6d6c1bb9609e3799693e1467e8e5558d9ec287
mauerbauertraurigkeit <nour20hk@gmail.com>
2021-12-06 23:12:11 +0200

    commit

--- app/src/main/java/com/nhk/unikit/ItemsPage.java
+++ app/src/main/java/com/nhk/unikit/ItemsPage.java
@@ -2,16 +2,29 @@ package com.nhk.unikit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 
 public class ItemsPage extends AppCompatActivity {
-    //ListView
+
+    Button addBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_page);
+        addBtn = (Button) findViewById(R.id.btn_add_item);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ItemsPage.this, AddItem.class));
+            }
+        });
 
     }
 }
