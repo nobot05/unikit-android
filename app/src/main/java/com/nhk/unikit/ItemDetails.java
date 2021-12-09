f4d6b34d455f03bf21b623f31614d636a0ba438a
mauerbauertraurigkeit <nour20hk@gmail.com>
2021-12-09 20:16:27 +0200

    commit

--- app/src/main/java/com/nhk/unikit/ItemDetails.java
+++ app/src/main/java/com/nhk/unikit/ItemDetails.java
@@ -2,13 +2,26 @@ package com.nhk.unikit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ItemDetails extends AppCompatActivity {
+
+    Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
+        backBtn = (Button) findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ItemDetails.this, ItemsPage.class));
+            }
+        });
     }
 }
