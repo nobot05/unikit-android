e1285dc23f1508ab46d0aff6bad1d6603e1c711a
mauerbauertraurigkeit <nour20hk@gmail.com>
2021-12-12 19:16:21 +0200

    commit

--- app/src/main/java/com/nhk/unikit/AddItem.java
+++ app/src/main/java/com/nhk/unikit/AddItem.java
@@ -7,20 +7,36 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AddItem extends AppCompatActivity {
@@ -35,13 +51,15 @@ public class AddItem extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
-    Button backBtn;
+    Button backBtn, addBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
         backBtn = (Button) findViewById(R.id.backBtn);
+        addBtn = (Button) findViewById(R.id.addBtn);
         addedImageBtn = (ImageView) findViewById(R.id.addedImage);
         addedImageBtn.setImageResource(R.drawable.folder);
 
@@ -55,6 +73,16 @@ public class AddItem extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 selectImage();
+            }
+        });
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BitmapDrawable drawable = (BitmapDrawable) addedImageBtn.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+                uploadImage(bitmap);
+                Log.e("Image Request","Done!");
             }
         });
 
@@ -117,6 +145,78 @@ public class AddItem extends AppCompatActivity {
                     PERMISSIONS_STORAGE,
                     REQUEST_EXTERNAL_STORAGE
             );
+        }
+    }
+
+    private void uploadImage(Bitmap imageBitmap){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.PNG, 90, stream);
+        byte[] b = stream.toByteArray();
+        String encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+        ArrayList<Pair<String, String>> params = new ArrayList<Pair<String, String>>();
+        params.add(new Pair<>("image", encodedImage));
+
+        try {
+            new AsyncUploader().execute("http://localhost/Nour-Final/addItem.php", getQuery(params));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getQuery(List<Pair<String, String>> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for(Pair<String, String> pair : params){
+            if(first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(pair.first, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(pair.second, "UTF-8"));
+        }
+        return result.toString();
+    }
+
+    private class AsyncUploader extends AsyncTask<String, Integer, String>
+    {
+        @Override
+        protected String doInBackground(String... strings) {
+            String urlString = strings[0];
+            String params = strings[1];
+            URL url = null;
+            InputStream stream = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(urlString);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+
+                urlConnection.connect();
+
+                OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+                wr.write(params);
+                wr.flush();
+
+                stream = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"), 8);
+                String result = reader.readLine();
+                return result;
+            }catch (IOException ioe){
+                ioe.printStackTrace();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected  void onPostExecute(String result) {
+            Toast.makeText(AddItem.this, result, Toast.LENGTH_SHORT).show();
         }
     }
 
